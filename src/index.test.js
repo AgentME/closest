0f4bfe3c39f67ad9f25576dbb5018a42d5f83e79
Chris Cowan <agentme49@gmail.com>
2016-11-16 19:03:37 -0800

    +test

---
+++ src/index.test.js
@@ -0,0 +1,40 @@
+/* @flow */
+
+import closest from './';
+
+document.body.innerHTML = `
+  <section id="a">
+    <div class="first">
+      <em>foo</em>
+      <span>spanner</span>
+    </div>
+    <div class="second">blah</div>
+  </section>
+  <section id="b">
+    <div class="first">
+      <strong>text</strong>
+    </div>
+  </section>
+`;
+
+// ['native', 'not native'].forEach(label => {
+//   describe(label, () => {
+test('works', () => {
+  expect(closest(document.querySelector('#a .first'), 'section')).toBe(
+    document.querySelector('#a')
+  );
+});
+
+test("doesn't find things it shouldn't", () => {
+  expect(closest(document.querySelector('#a .first'), '.second')).toBe(
+    null
+  );
+});
+
+test('can match self', () => {
+  expect(closest(document.querySelector('#b .first'), '.first')).toBe(
+    document.querySelector('#b .first')
+  );
+});
+//   });
+// });
